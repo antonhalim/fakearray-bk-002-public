9d227ac765cc60b7af2c842bbf93cb43109b12cb
axton21 <axton_n@yahoo.com>
2015-02-12 12:27:01 -0500

    its not fake at the end

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,24 @@
 class FakeArray
+  require 'pry'
 
-  # you'll need a splat in this class somewhere
+  def initialize(data1, data2, data3)
+    @data = []
+    @data << data1 << data2 << data3
+  end
 
+  def [](index)
+    @data[index]
+  end
+
+  def each
+    @data.each do |item|
+    yield item
+    end
+  end
+
+  def first
+    @data[0]
+  end
 
 
 end
